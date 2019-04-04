c3559e86c7bdcb1f96b15f0f35b27433c8ef02df
zeleznicka <gosiekx3@op.pl>
2019-04-04 11:47:03 +0200

    lab6

--- calc.js
+++ calc.js
@@ -1,9 +1,41 @@
+const pola = [
+    { txt: 1, row: 4, col: 1 },
+    { txt: 2, row: 4, col: 2 },
+    { txt: 3, row: 4, col: 3 },
+    { txt: 4, row: 3, col: 1 },
+    { txt: 5, row: 3, col: 2 },
+    { txt: 6, row: 3, col: 3 },
+    { txt: 7, row: 2, col: 1 },
+    { txt: 8, row: 2, col: 2 },
+    { txt: 9, row: 2, col: 3 },
+    { txt: 'C', row: 2, col: 4 },
+    { txt: '+', row: 3, col: 4 },
+    { txt: '-', row: 4, col: 4 },
+    { txt: '=', row: 5, col: 4 },
+    { txt: '.', row: 5, col: 3 },
+    { txt: '0', row: 5, col: '1/3' },
+    { txt: 'Display', row: 1, col: '1/5' }];
 
-const double = x =>2*x;
-const init = (e,x)=> {
-    console.error(typeof e);
-    console.info(double(x));
-    console.log('Container:',document.getElementById('container'));
+const init = () => {
+    const container = document.createElement('div');
+    container.id = 'container';
+    pola.forEach(el => {
+        const div = document.createElement('div');
+        div.textContent = el.txt;
+        div.style.gridColumn = el.col;
+        div.style.gridRow = el.row;
+        if (el.txt === 'Display') {
+            div.id = 'display';
+        } else {
+            div.addEventListener('click', ev => {
+                const d = document.getElementById('display');
+                d.textContent = ev.target.textContent;
+            });
+        }
+        container.appendChild(div);
+    });
+    document.body.appendChild(container);
+    //for(let i=0; i<pola.length; ++i){
+    //    console.log(pola[i]); }
 }
-window.addEventListener('DOMContentLoaded', e=>init(e,5));
-
+window.addEventListener('DOMContentLoaded', init);
